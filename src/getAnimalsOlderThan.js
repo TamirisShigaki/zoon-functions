27bf31f5e7f5ff5fd19389555db65378ae658821
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-25 18:11:02 -0300

    requisito 2: ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specie = species.find((animais) => animais.name === animal);
+  return specie.residents.every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
@@ -9,3 +8,7 @@ console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
+
+//! Referencia
+
+// .includes https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
