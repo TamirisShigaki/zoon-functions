be3e0f0cab8afc874ff46691a6ffd3219fcb5f80
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-25 16:13:26 -0300

    requisito 1: ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código
-}
+const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
