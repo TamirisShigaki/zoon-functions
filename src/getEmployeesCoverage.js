32f92faf2c14a4d5eedb4e56fcbb5b247d7d46b4
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-27 15:24:19 -0300

    requisito 10: ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getNameSpecie(idAnimal) {
+  return data.species.find((animal) => animal.id === idAnimal).name;
+}
+
+function getLocationsSpecie(idAnimal) {
+  return data.species.find((animal) => animal.id === idAnimal).location;
+}
+
+function montandoObject(objectEmployee) {
+  return {
+    id: objectEmployee.id,
+    fullName: `${objectEmployee.firstName} ${objectEmployee.lastName}`,
+    species: objectEmployee.responsibleFor.map((id) => getNameSpecie(id)),
+    locations: objectEmployee.responsibleFor.map((id) => getLocationsSpecie(id)),
+  };
+}
+
+function verificaEmployee(employee) {
+  return data.employees.find((empregado) => {
+    if (empregado.id === employee.id) return true;
+    if (empregado.firstName === employee.name) return true;
+    if (empregado.lastName === employee.name) return true;
+    return false;
+  });
+}
+
+function getEmployeesCoverage(employee) {
+  if (!employee) {
+    return data.employees.map((empregado) => montandoObject(empregado));
+  }
+
+  const employeeEncontrado = verificaEmployee(employee);
+
+  if (!employeeEncontrado) {
+    throw new Error('Informações inválidas');
+  }
+  const resposta = montandoObject(employeeEncontrado);
+  return resposta;
 }
 
 module.exports = getEmployeesCoverage;
+
+//! Ajuda Do Marcelo Adriano Turma 15 - Tribo A
