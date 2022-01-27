fdae5234b8ba18e7339d2da9f3f007d87488d0d8
Tamiris Shigaki <shigakiendo@gmail.com>
2022-01-26 22:09:19 -0300

    requisito 9: ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((element) => element.id === id);
+
+  const idAnimal = employee.responsibleFor[0];
+
+  const animal = data.species.find((specie) => specie.id === idAnimal);
+
+  const olderAge = animal.residents.reduce((acc, current) => {
+    if (current.age > acc.age) return current;
+    return acc;
+  });
+  return Object.values(olderAge);
 }
 
 module.exports = getOldestFromFirstSpecies;
